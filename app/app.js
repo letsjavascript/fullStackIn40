c9c3d312b6e1565a9aec721510a04971273ee83f
dpollan <davidpollan@gmail.com>
2018-03-19 22:36:46 -0500

    Establish initial routes and add body parser middleware for forms

--- app/app.js
+++ app/app.js
@@ -1,8 +1,45 @@
 const Express = require('express');
 const app = Express();
+const bodyParser = require('body-parser');
 const config = require('../config/config.js');
 
-app.get('/', (req,res)=>{res.send('Yee-Haw, basic server')});
+app.use(bodyParser.urlencoded({extended: true}));
 
+//set up routes
+
+//home
+app.get('/', (req,res) => {
+	res.send('Yee-Haw, basic server')
+});
+
+//add user data
+app.get('/addSomeData', (req,res) => {
+		console.log('TODO: Send the user a html form that they can fill in');
+		res.send('Add Data');
+});
+
+app.post('/addSomeData', (req,res) => {
+		data = req.body;
+		console.log(data);
+		console.log('TODO: put some data in the database using knex and inform the user');
+		res.send('Add Data');
+});
+
+//delete user data
+app.post('deleteSomeData', (req,res) => {
+	dataToDelete = req.body;
+	console.log(dataToDelete);
+	console.log('TODO: Send the user a confirmation that they are about to delete this data');
+	console.log('Then if they confirm send knex call to delete user');
+	res.send('Delete Data');
+});
+
+//view user data
+app.get('/seeSomeData', (req,res) => {
+	console.log('TODO: Create a frontend that displays all the data from db');
+	res.send('View Data');
+});
+
+//start the server
 console.log('Server listening on port ' + config.server.port);
 app.listen(config.server.port);
